ba607d0fa97acccec5fc16b95c97cb3031dc0c05
LukeEising <LukeEising.DaanEising@gmail.com>
2025-06-05 10:51:48 +0200

    Data (2) inladen

--- luke.R
+++ luke.R
@@ -8,3 +8,13 @@ Welzijn <- read_csv2("C:/Users/Luke Eising/Downloads/85542NED_UntypedDataSet_030
 
 Welzijn$ScoreGeluk_1
 write.csv(Welzijn, "data/Welzijn.csv")
+
+Welzijn <- read_csv("data/Welzijn.csv")
+
+library(readr)
+Levensverwachting <- read_delim("C:/Users/Luke Eising/Downloads/85445NED_UntypedDataSet_03062025_092005.csv", 
+                                delim = ";", escape_double = FALSE, trim_ws = TRUE)
+
+write.csv(Levensverwachting, "data/Levensverwachting.csv")
+
+Levensverwachting <- read_csv("data/Levensverwachting.csv")
