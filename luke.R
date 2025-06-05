0045193f2c33439fc1bfaa58b868801b5c6c0d3e
LukeEising <LukeEising.DaanEising@gmail.com>
2025-06-05 10:45:30 +0200

    Data inladen

--- luke.R
+++ luke.R
@@ -1,1 +1,10 @@
-m1 = matrix(1:6, nrow = 2, ncol = 3)
+#####| |\   | | ----- #####
+#####| | \  | |   |   #####
+#####| |  \ | |   |   #####
+#####| |   \| |   |   #####
+
+library(readr)
+Welzijn <- read_csv2("C:/Users/Luke Eising/Downloads/85542NED_UntypedDataSet_03062025_093918.csv")
+
+Welzijn$ScoreGeluk_1
+write.csv(Welzijn, "data/Welzijn.csv")
